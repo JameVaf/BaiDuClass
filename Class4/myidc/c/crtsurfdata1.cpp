94696dd9cb2f78acf3cfa0e9900f5fba87167e90
JameVaf <1975953451@qq.com>
2024-03-17 16:24:33 +0800

    code litele Class4

---
+++ Class4/myidc/c/crtsurfdata1.cpp
@@ -0,0 +1,15 @@
+/*
+ *   程序名:crtsurfdata1.cpp 用于生成全国气象站观测的分钟数据
+*/
+
+#include "_public.h"
+
+int main(int argc,char* argv[])
+{
+    //程序需要的参数 iniFile outPath lohFile
+    if(argc != 4)
+    {
+        
+    }
+    return 0;
+}
